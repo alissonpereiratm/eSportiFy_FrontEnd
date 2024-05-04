a1b8982bb300d21200a430c2117f1a20721ec75d
Alisson <alissonpereiratm@gmail.com>
2024-05-04 17:14:32 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+
+import { StyleSheet, Text, View, Image } from 'react-native';
+import ListaJogos from './components/ListaJogos';
+import LoginUsuario from './components/Login/LoginUsuario';
+import CadastroUsuario from './components/CadastroUsuario';
+import AgendaJogos from './components/AgendaJogos';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Tab = createBottomTabNavigator();
+
+//botões de navegação inferior
+function MyTabs() {
+  return (
+    
+    <Tab.Navigator screenOptions={{headerTransparent : true, title: "", tabBarActiveTintColor: '#000'}}>
+      <Tab.Screen name="Lista Jogos"  component={ListaJogos} options={{
+        tabBarIcon: () => (
+          <Image
+            style={{ width: 30, height: 30}}
+            source={require('../img/homeIcon.png')
+          }/>
+         ),
+         tabBarLabel: "home"
+        }}
+      />
+
+      <Tab.Screen name="Login" component={LoginUsuario} options={{
+        tabBarIcon: () =>(
+          <Image
+            style= {{width:30, height:30, marginTop:5, marginLeft:5}}
+            source={require('../img/Agenda.png')
+            }/>
+          ),
+          tabBarLabel: "agenda"
+        }} 
+      />
+    </Tab.Navigator>
+  );
+}
+
+//container de navegação
+export default function App() {
+  return(
+    <LoginUsuario/>
+  );
+}
+
+
+
+
+
